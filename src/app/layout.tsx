e2db6ee94be84e26fc467c86f3976b66d3b25e94
geetam <geetambinani6@gmail.com>
2025-03-16 18:33:48 +0530

    initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: {
+    template: "%s - AI Resume Builder",
+    absolute: "AI Resume Builder",
+  },
+  description:
+    "AI Resume Builder is the easiest way to create a resume with AI that will help you to land your dream job",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
